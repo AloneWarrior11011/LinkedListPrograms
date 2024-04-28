3c78d969a3e21b0a2f326f95ea9ac3348e6dae91
Kunal Subhash Wadhai <123325606+AloneWarrior11011@users.noreply.github.com>
2024-04-29 00:47:24 +0530

    Create Remove every kth node.cpp
    Signed-off-by: Kunal Subhash Wadhai <123325606+AloneWarrior11011@users.noreply.github.com>

---
+++ Remove every kth node.cpp
@@ -0,0 +1,120 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+
+void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    
+    *tail_ref = new_node;
+}
+
+
+// } Driver Code Ends
+/* Link list Node 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+
+/*You are required to complete this method*/
+
+/* Function to get the middle of the linked list*/
+/*Input:
+n = 8
+linked list: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 
+k = 2
+Output: 
+1 -> 3 -> 5 -> 7
+Explanation: 
+After removing every 2nd node of the linked list, the resultant linked list will be: 1 -> 3 -> 5 -> 7.*/
+
+class Solution {
+    public:
+    Node* deleteK(Node *head,int K){
+      //Your code here
+      if(head == NULL || K < 0) return head;
+      
+      if(K == 1) return NULL;
+      
+      int counter = 1;
+      
+      Node* prev = NULL;
+      Node* curr = head;
+      
+      while(curr){
+          if(counter == K){
+             prev->next = curr->next;
+             curr = curr->next;
+             counter = 1;
+          }else{
+              prev = curr;
+              curr = curr->next;
+              counter++;
+          }
+      }
+      return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l;
+    cin>>T;
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+        int K;
+        cin>>K;
+        Solution obj;
+        Node *res = obj.deleteK(head,K);
+        Node *temp = res;
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
